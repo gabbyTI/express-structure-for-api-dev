abd3d2870094621947fde8b5db36328ff69d4629
Gabriel Ibenye <gabrielibenye@gmail.com>
2022-03-21 02:57:31 +0100

    security update: added express rate limiter

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const helloWorldRouter = require('./routes/helloWorld');
 const globalErrorHandler = require('./exceptions/handler');
 
@@ -12,6 +14,16 @@ console.log(`You are in ${process.env.NODE_ENV} environment.`);
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev')); // prints the request info
 }
+
+// Limits number of request from an IP
+const limiter = rateLimit({
+	max: 100, // no of request
+	windowMs: 60 * 60 * 1000, // reset time in milliseconds (1 hour)
+	message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+// Applying the limiter to onlyapi routes
+app.use('/api',limiter);
 
 // allows a post body to be added to the request object
 app.use(express.json());
