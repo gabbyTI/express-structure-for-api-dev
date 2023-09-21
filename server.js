a262a8f54904728a4942feea128dda8ec8ca2f31
Gabriel Ibenye <gabrielibenye@gmail.com>
2023-09-21 12:25:13 +0100

    optimized server js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+// This loads data from the config.env file into the application
+dotenv.config({ path: './config.env' });
 
 process.on('uncaughtException', (err) => {
 	console.log('UNCAUGHT EXCEPTION! Shutting down...');
@@ -7,31 +10,7 @@ process.on('uncaughtException', (err) => {
 	process.exit(1);
 });
 
-// This loads data from the config.env file into the application
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
-
-/** Connecting to Atlas DB
-
-    const DB_CONNECTION_STRING = process.env.DATABASE.replace(
-        '<PASSWORD>',
-        encodeURIComponent(process.env.DATABASE_PASSWORD)
-    );
-
-    mongoose.connect(DB_CONNECTION_STRING).then(() => {
-        console.log('DB Connection Successful');
-    });
-
-*/
-
-/** Connecting to local DB
- 
-    mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
-        console.log('DB Connection Successful');
-    });
-
- */
 
 const port = process.env.PORT || 4000;
 
